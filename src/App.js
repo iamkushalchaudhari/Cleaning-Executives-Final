a126a9c021854005a6ac3c19d54c1a41049e0c94
Kushal Chaudhari <chaudharikushalganesh@gmail.com>
2022-10-13 19:29:01 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Features from './pages/Features';
-import './App.css';
 import Landing from './pages/Landing';
 import GetNotified from './pages/GetNotified';
+import LoginPage from './pages/LoginPage';
+import './App.css';
 export default function App() {
   return (
     <>
    
-    <Landing/>
+    {/* <Landing/>
     <Features/>
-    <GetNotified/>
+    <GetNotified/> */}
+    <LoginPage/>
+    
     </>
   )
 }
