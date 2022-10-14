5b12b2a5327ba649ad6b0c04259125a3fa17603a
Kushal Chaudhari <chaudharikushalganesh@gmail.com>
2022-10-14 07:46:48 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import './App.css';
 import Features from './pages/Features';
 import Landing from './pages/Landing';
 import GetNotified from './pages/GetNotified';
 import LoginPage from './pages/LoginPage';
-import './App.css';
+import BuySell from './pages/BuySell';
 export default function App() {
   return (
     <>
    
     {/* <Landing/>
     <Features/>
-    <GetNotified/> */}
-    <LoginPage/>
-    
+    <GetNotified/> 
+    <LoginPage/>*/}
+    <BuySell/>
     </>
   )
 }
---
+++ src/pages/BuySell.jsx
@@ -0,0 +1,167 @@
+export default function Example () {
+    return (
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+          <div className="flex">
+            <div className="pt-1 mr-6 text-center">
+              <div className="px-2 pb-1 mb-1 border-b border-gray-400">
+                <p className="text-sm text-blue-gray-700">Jul</p>
+              </div>
+              <div className="px-2">
+                <p className="text-lg font-bold">18</p>
+              </div>
+            </div>
+            <div>
+              <div className="mb-2">
+                <a
+                  href="/"
+                  className="text-xs font-semibold tracking-wide uppercase transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                  aria-label="Category"
+                  title="Plastic Waste"
+                >
+                  Plastic Waste
+                </a>
+              </div>
+              <div className="mb-2">
+                <a
+                  href="/"
+                  aria-label="Article"
+                  className="inline-block text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+                >
+                 Avaliable Plastic Waste.
+                </a>
+              </div>
+              <p className="mb-5 text-gray-700">
+                I have Carry bags as waste avaliable for sell anytime.  
+              </p>
+              <div className="flex items-center">
+                <a href="/" aria-label="People" title="People" className="mr-3">
+                  <img
+                    src="https://drive.google.com/uc?export=view&id=1jYNkFbSDzOlgHvTXc2nEk8UQQ9ytexy-"
+                    alt="avatar"
+                    className="object-cover w-10 h-10 rounded-full shadow-sm"
+                  />
+                </a>
+                <div>
+                  <a
+                    href="/"
+                    aria-label="People"
+                    title="People"
+                    className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                  >
+                    Petru Vîrtos
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="pt-1 mr-6 text-center">
+              <div className="px-2 pb-1 mb-1 border-b border-gray-400">
+                <p className="text-sm text-blue-gray-700">May</p>
+              </div>
+              <div className="px-2">
+                <p className="text-lg font-bold">6</p>
+              </div>
+            </div>
+            <div>
+              <div className="mb-2">
+                <a
+                  href="/"
+                  className="text-xs font-semibold tracking-wide uppercase transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                  aria-label="Category"
+                  title="Plastic &amp; Vegetable Waste"
+                >
+                  Plastic &amp; Vegetable Waste 
+                </a>
+              </div>
+              <div className="mb-2">
+                <a
+                  href="/"
+                  aria-label="Article"
+                  className="inline-block text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+                >
+                  Both Plastic &amp; Vegetable.
+                </a>
+              </div>
+              <p className="mb-5 text-gray-700">
+                I am having packaging bags and daily vegetable waste for sell.
+              </p>
+              <div className="flex items-center">
+                <a href="/" aria-label="People" title="People" className="mr-3">
+                  <img
+                    src="https://drive.google.com/uc?export=view&id=1K6qKowFb5_qkcUqCD9WcdDjQocR9ZPkr"
+                    alt="avatar"
+                    className="object-cover w-10 h-10 rounded-full shadow-sm"
+                  />
+                </a>
+                <div>
+                  <a
+                    href="/"
+                    aria-label="People"
+                    title="People"
+                    className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                  >
+                    Alexa Stratulat
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="pt-1 mr-6 text-center">
+              <div className="px-2 pb-1 mb-1 border-b border-gray-400">
+                <p className="text-sm text-blue-gray-700">Feb</p>
+              </div>
+              <div className="px-2">
+                <p className="text-lg font-bold">27</p>
+              </div>
+            </div>
+            <div>
+              <div className="mb-2">
+                <a
+                  href="/"
+                  className="text-xs font-semibold tracking-wide uppercase transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                  aria-label="Category"
+                  title="Glass Waste"
+                >
+                  Glass Waste
+                </a>
+              </div>
+              <div className="mb-2">
+                <a
+                  href="/"
+                  aria-label="Article"
+                  className="inline-block text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+                >
+                  Just Glass Waste.
+                </a>
+              </div>
+              <p className="mb-5 text-gray-700">
+              I have some glass waste of 3 wine glasses and one artifact.
+              </p>
+              <div className="flex items-center">
+                <a href="/" aria-label="People" title="People" className="mr-3">
+                  <img
+                    src="https://drive.google.com/uc?export=view&id=158W9637gcs5CEhkVs0lVk8C4uIDgb5Tl"
+                    alt="avatar"
+                    className="object-cover w-10 h-10 rounded-full shadow-sm"
+                  />
+                </a>
+                <div>
+                  <a
+                    href="/"
+                    aria-label="People"
+                    title="People"
+                    className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                  >
+                    Susie the Cat
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
