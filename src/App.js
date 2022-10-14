486a79a66adf2a7096f956bfb9a1e8ee0ed4cfc4
Kushal Chaudhari <chaudharikushalganesh@gmail.com>
2022-10-14 11:36:10 +0530

    Routes Added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+// import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import './App.css';
 import Features from './pages/Features';
 import Landing from './pages/Landing';
 import GetNotified from './pages/GetNotified';
 import LoginPage from './pages/LoginPage';
 import BuySell from './pages/BuySell';
+import { 
+  BrowserRouter as Router, 
+  Routes,
+  Route, 
+  Link 
+} from 'react-router-dom';
+
 export default function App() {
   return (
-    <>
-   
-    {/* <Landing/>
-    <Features/>
-    <GetNotified/> 
-    <LoginPage/>*/}
-    <BuySell/>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path ='/' element={<Landing/>} />
+        <Route exact path ='/features' element={<Features/>} />
+        <Route exact path ='/getnotified' element={<GetNotified/>} /> 
+        <Route exact path ='/login' element={<LoginPage/>} /> 
+      </Routes>
+      {/* <LoginPage/> */}
+      {/* <BuySell/> */}
+    </Router>
   )
 }
 
--- src/pages/BuySell.jsx
+++ src/pages/BuySell.jsx
@@ -1,60 +1,122 @@
-export default function Example () {
-    return (
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
-          <div className="flex">
-            <div className="pt-1 mr-6 text-center">
-              <div className="px-2 pb-1 mb-1 border-b border-gray-400">
-                <p className="text-sm text-blue-gray-700">Jul</p>
-              </div>
-              <div className="px-2">
-                <p className="text-lg font-bold">18</p>
-              </div>
+export default function Example() {
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+       <div>
+        <div className="flex">
+          <div className="pt-1 mr-6 text-center">
+            <div className="px-2 pb-1 mb-1 border-b border-gray-400">
+              <p className="text-sm text-blue-gray-700">Jul</p>
             </div>
-            <div>
-              <div className="mb-2">
-                <a
-                  href="/"
-                  className="text-xs font-semibold tracking-wide uppercase transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-                  aria-label="Category"
-                  title="Plastic Waste"
-                >
-                  Plastic Waste
-                </a>
-              </div>
-              <div className="mb-2">
-                <a
-                  href="/"
-                  aria-label="Article"
-                  className="inline-block text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
-                >
-                 Avaliable Plastic Waste.
-                </a>
-              </div>
-              <p className="mb-5 text-gray-700">
-                I have Carry bags as waste avaliable for sell anytime.  
-              </p>
-              <div className="flex items-center">
-                <a href="/" aria-label="People" title="People" className="mr-3">
-                  <img
-                    src="https://drive.google.com/uc?export=view&id=1jYNkFbSDzOlgHvTXc2nEk8UQQ9ytexy-"
-                    alt="avatar"
-                    className="object-cover w-10 h-10 rounded-full shadow-sm"
-                  />
-                </a>
-                <div>
-                  <a
-                    href="/"
-                    aria-label="People"
-                    title="People"
-                    className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                  >
-                    Petru Vîrtos
-                  </a>
-                </div>
-              </div>
+            <div className="px-2">
+              <p className="text-lg font-bold">18</p>
             </div>
           </div>
+          <div>
+            <div className="mb-2">
+              <a
+                href="/"
+                className="text-xs font-semibold tracking-wide uppercase transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                aria-label="Category"
+                title="Plastic Waste"
+              >
+                Plastic Waste
+              </a>
+            </div>
+            <div className="mb-2">
+              <a
+                href="/"
+                aria-label="Article"
+                className="inline-block text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+              >
+                Avaliable Plastic Waste.
+              </a>
+            </div>
+            <p className="mb-5 text-gray-700">
+              I have Carry bags as waste avaliable for sell anytime.
+            </p>
+            <div className="flex items-center">
+              <a href="/" aria-label="People" title="People" className="mr-3">
+                <img
+                  src="https://drive.google.com/uc?export=view&id=1jYNkFbSDzOlgHvTXc2nEk8UQQ9ytexy-"
+                  alt="avatar"
+                  className="object-cover w-10 h-10 rounded-full shadow-sm"
+                />
+              </a>
+              <div className="pr-3">
+                <a
+                  href="/"
+                  aria-label="People"
+                  title="People"
+                  className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                >
+                  Petru Vîrtos
+                </a>
+              </div>
+              <button type="Contact" class="text-white tracking-wide bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-base px-4 py-2 mr-2 mb-2 ">Contact</button>
+            </div>
+          </div>
+        </div>
+        
+        <div className="flex pt-6">
+          <div className="pt-1 mr-6 text-center">
+            <div className="px-2 pb-1 mb-1 border-b border-gray-400">
+              <p className="text-sm text-blue-gray-700">Jul</p>
+            </div>
+            <div className="px-2">
+              <p className="text-lg font-bold">8</p>
+            </div>
+          </div>
+          <div>
+            <div className="mb-2">
+              <a
+                href="/"
+                className="text-xs font-semibold tracking-wide uppercase transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                aria-label="Category"
+                title="Plastic Waste &amp; Paper Waste"
+              >
+                Plastic Waste &amp; Paper Waste.
+              </a>
+            </div>
+            <div className="mb-2">
+              <a
+                href="/"
+                aria-label="Article"
+                className="inline-block text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+              >
+                Plastic &amp; Paper.
+              </a>
+            </div>
+            <p className="mb-5 text-gray-700">
+              I have some cutleries of package food and paper.
+            </p>
+            <div className="flex items-center">
+              <a href="/" aria-label="People" title="People" className="mr-3">
+                <img
+                  src="https://drive.google.com/uc?export=view&id=1DRK0ypkkDJqOq1jHlwIgcTw447H-3qaI"
+                  alt="avatar"
+                  className="object-cover w-10 h-10 rounded-full shadow-sm"
+                />
+              </a>
+              <div className="pr-3">
+                <a
+                  href="/"
+                  aria-label="People"
+                  title="People"
+                  className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                >
+                  Bob The Builder
+                </a>
+              </div>
+              <button type="Contact" class="text-white tracking-wide bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-base px-4 py-2 mr-2 mb-2 ">Contact</button>
+            </div>
+          </div>
+        </div>
+        
+        </div>
+
+       {/* Vegetable Waste */}
+        <div>
           <div className="flex">
             <div className="pt-1 mr-6 text-center">
               <div className="px-2 pb-1 mb-1 border-b border-gray-400">
@@ -72,7 +134,7 @@ export default function Example () {
                   aria-label="Category"
                   title="Plastic &amp; Vegetable Waste"
                 >
-                  Plastic &amp; Vegetable Waste 
+                  Plastic &amp; Vegetable Waste
                 </a>
               </div>
               <div className="mb-2">
@@ -95,7 +157,7 @@ export default function Example () {
                     className="object-cover w-10 h-10 rounded-full shadow-sm"
                   />
                 </a>
-                <div>
+                <div className="pr-3">
                   <a
                     href="/"
                     aria-label="People"
@@ -105,10 +167,70 @@ export default function Example () {
                     Alexa Stratulat
                   </a>
                 </div>
+                <button type="Contact" class="text-white tracking-wide bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-base px-4 py-2 mr-2 mb-2 ">Contact</button>
               </div>
             </div>
           </div>
-          <div className="flex">
+
+          <div className="flex pt-6">
+            <div className="pt-1 mr-6 text-center">
+              <div className="px-2 pb-1 mb-1 border-b border-gray-400">
+                <p className="text-sm text-blue-gray-700">April</p>
+              </div>
+              <div className="px-2">
+                <p className="text-lg font-bold">26</p>
+              </div>
+            </div>
+            <div>
+              <div className="mb-2">
+                <a
+                  href="/"
+                  className="text-xs font-semibold tracking-wide uppercase transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                  aria-label="Category"
+                  title="Plastic Waste"
+                >
+                  Plastic Waste
+                </a>
+              </div>
+              <div className="mb-2">
+                <a
+                  href="/"
+                  aria-label="Article"
+                  className="inline-block text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+                >
+                  Only Plastic Waste.
+                </a>
+              </div>
+              <p className="mb-5 text-gray-700">
+                Only plastic waste like wrappers, milk pouches like that.
+              </p>
+              <div className="flex items-center">
+                <a href="/" aria-label="People" title="People" className="mr-3">
+                  <img
+                    src="https://drive.google.com/uc?export=view&id=10QdZjqSZUmUQns_OPUf933we6w65gSc-"
+                    alt="avatar"
+                    className="object-cover w-10 h-10 rounded-full shadow-sm"
+                  />
+                </a>
+                <div className="pr-3">
+                  <a
+                    href="/"
+                    aria-label="People"
+                    title="People"
+                    className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                  >
+                    John Doe
+                  </a>
+                </div>
+                <button type="Contact" class="text-white tracking-wide bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-base px-4 py-2 mr-2 mb-2 ">Contact</button>
+              </div>
+            </div>
+            </div>
+        </div>
+
+       {/* Glass Waste */}
+        <div>
+          <div className="flex ">
             <div className="pt-1 mr-6 text-center">
               <div className="px-2 pb-1 mb-1 border-b border-gray-400">
                 <p className="text-sm text-blue-gray-700">Feb</p>
@@ -138,7 +260,7 @@ export default function Example () {
                 </a>
               </div>
               <p className="mb-5 text-gray-700">
-              I have some glass waste of 3 wine glasses and one artifact.
+                I have some glass waste of 3 wine glasses and one artifact.
               </p>
               <div className="flex items-center">
                 <a href="/" aria-label="People" title="People" className="mr-3">
@@ -148,7 +270,62 @@ export default function Example () {
                     className="object-cover w-10 h-10 rounded-full shadow-sm"
                   />
                 </a>
-                <div>
+                <div className="pr-3">
+                  <a
+                    href="/"
+                    aria-label="People"
+                    title="People"
+                    className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                  >
+                    Mark Ruffalo
+                  </a>
+                </div>
+                <button type="Contact" class="text-white tracking-wide bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-base px-4 py-2 mr-2 mb-2 ">Contact</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex pt-6">
+            <div className="pt-1 mr-6 text-center">
+              <div className="px-2 pb-1 mb-1 border-b border-gray-400">
+                <p className="text-sm text-blue-gray-700">Feb</p>
+              </div>
+              <div className="px-2">
+                <p className="text-lg font-bold">12</p>
+              </div>
+            </div>
+            <div>
+              <div className="mb-2">
+                <a
+                  href="/"
+                  className="text-xs font-semibold tracking-wide uppercase transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                  aria-label="Category"
+                  title="Glass Waste"
+                >
+                  Glass Waste
+                </a>
+              </div>
+              <div className="mb-2">
+                <a
+                  href="/"
+                  aria-label="Article"
+                  className="inline-block text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
+                >
+                  Glass Waste.
+                </a>
+              </div>
+              <p className="mb-5 text-gray-700">
+                I have old glass utensils like Wine glass and some.
+              </p>
+              <div className="flex items-center">
+                <a href="/" aria-label="People" title="People" className="mr-3">
+                  <img
+                    src="https://drive.google.com/uc?export=view&id=1fjEY3yNN46B4gwnAUMrgXus2xmKJQle2"
+                    alt="avatar"
+                    className="object-cover w-10 h-10 rounded-full shadow-sm"
+                  />
+                </a>
+                <div className="pr-3">
                   <a
                     href="/"
                     aria-label="People"
@@ -158,10 +335,12 @@ export default function Example () {
                     Susie the Cat
                   </a>
                 </div>
+                <button type="Contact" class="text-white tracking-wide bg-indigo-600 hover:bg-indigo-700 font-medium rounded-lg text-base px-4 py-2 mr-2 mb-2 ">Contact</button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
