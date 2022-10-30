a1754713e74e4819d66ea8763004f59604682ada
Kushal Chaudhari <chaudharikushalganesh@gmail.com>
2022-10-30 20:55:11 +0530

    Adding countdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,23 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      width: {
-        '128': '32rem',
-      }
-    },
-  },
+          extend: {
+                  animation: { flip: 'flip 1s cubic-bezier(0, 0, 0.2, 1) infinite' },
+                  width: { '128': '32rem' },
+                  keyframes: {
+                              flip: {
+                                      'from': { transform: 'rotateX(0deg)', transformOrigin: '50% bottom ', },
+                                      'to': { transform: 'rotateX(180deg)', transformOrigin: '50% bottom ', }
+                                      }
+                              },
+                    fontFamily: {'redhat': ['Red Hat Text', 'sans-serif']},
+                  },
+         },
   plugins: [
-    require('tailwind-scrollbar'),
-  ],
+             require('tailwind-scrollbar'),
+            ],
 }
